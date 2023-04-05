b273bd95c124ea84a8209e54f4a92378f11426a3
feliz-pongo <felizpongo980@gmail.com>
2023-04-05 13:56:25 +0100

    commit 1

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>69ae6667-f11c-465d-ab42-bc27c28597ba</testSuiteGuid>
+</TestSuiteEntity>
