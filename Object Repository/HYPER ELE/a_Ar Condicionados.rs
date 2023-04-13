b332b7b5d9bcdf05a19cf8c8a668ecf7769d267f
feliz-pongo <felizpongo980@gmail.com>
2023-04-13 15:09:37 +0100

    commit 14

---
+++ Object Repository/HYPER ELE/a_Ar Condicionados.rs
@@ -0,0 +1,131 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Ar Condicionados</name>
+   <tag></tag>
+   <elementGuidId>5eebda5f-c28a-41be-ad32-6d47fd050901</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#menu-categorias-hiper-1 > #menu-item-6255 > a[title=&quot;Ar Condicionados&quot;]</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//li[@id='menu-item-6255']/a)[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>85d05cee-6053-4153-982d-0e1ac8d21ff4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>title</name>
+      <type>Main</type>
+      <value>Ar Condicionados</value>
+      <webElementGuid>2f59f47b-1e1d-491d-8f19-fa2adcc278ea</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>https://hiper.ao/product-category/ar-condicionados/</value>
+      <webElementGuid>359cdc51-419e-4814-9d77-f8acb1421a7c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Ar Condicionados</value>
+      <webElementGuid>5b28ad94-1856-4fd5-aa9d-9d9e3ee757fd</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;menu-categorias-hiper-1&quot;)/li[@id=&quot;menu-item-6255&quot;]/a[1]</value>
+      <webElementGuid>50a219c7-a586-4624-9538-057c9657be52</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>(//li[@id='menu-item-6255']/a)[2]</value>
+      <webElementGuid>8cfe2261-1fb6-4a95-91f0-96816dd7982d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>(//a[contains(text(),'Ar Condicionados')])[2]</value>
+      <webElementGuid>a7559382-25a9-4e44-ac4b-badf5c208a9f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='CATEGORIAS'])[1]/following::a[1]</value>
+      <webElementGuid>71a66177-7140-42d9-b61e-5afea3d9fcb3</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Email: hiper@icc-angola.com'])[1]/following::a[2]</value>
+      <webElementGuid>30743a40-5de6-4842-b7e5-beaab939e9f5</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Arcas'])[2]/preceding::a[1]</value>
+      <webElementGuid>497069ea-d4d8-4641-92a7-49ccac5e504a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Balanças'])[2]/preceding::a[2]</value>
+      <webElementGuid>e2d0371c-bce6-455f-8f39-890978fe238c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>(//a[contains(@href, 'https://hiper.ao/product-category/ar-condicionados/')])[2]</value>
+      <webElementGuid>9b3d512c-6aea-46e3-a762-aa193d6e84c8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/div/div/div/ul/li/a</value>
+      <webElementGuid>e1c675e8-f7da-42d6-811d-f91b2ad44227</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@title = 'Ar Condicionados' and @href = 'https://hiper.ao/product-category/ar-condicionados/' and (text() = 'Ar Condicionados' or . = 'Ar Condicionados')]</value>
+      <webElementGuid>c2d4f760-1837-4832-8e63-3f05c063af0e</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/Hyper_Electico/HYP_AR.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>HYP_AR</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>2c9d8f71-a626-4715-8095-b517a75cbb2f</testSuiteGuid>
+   <testCaseLink>
+      <guid>0587cb3f-c5b0-4fce-aa07-5e71e86803ad</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/HYE/HYPER</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
