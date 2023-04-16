e5f167d1d8489b64fc622eaa495a36ea04f558d5
feliz-pongo <felizpongo980@gmail.com>
2023-04-16 22:58:42 +0100

    commit 20

---
+++ Test Suites/Folha8_Suite/Folha_Rodar.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Folha_Rodar</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>db12a7aa-416b-4865-8c57-82d18c6a301d</testSuiteGuid>
+   <testCaseLink>
+      <guid>dc72bb3f-03eb-4050-a437-307cdf59123d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Folha8 TC/Folha</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
