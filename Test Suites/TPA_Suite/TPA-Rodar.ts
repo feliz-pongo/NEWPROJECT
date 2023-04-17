f5354e468db30329bfc352336f5eba5c23c12c94
feliz-pongo <felizpongo980@gmail.com>
2023-04-17 13:38:30 +0100

    commit 23

---
+++ Test Suites/TPA_Suite/TPA-Rodar.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TPA-Rodar</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>0a7bde95-9067-455d-b20d-6d9e88cf7a7f</testSuiteGuid>
+   <testCaseLink>
+      <guid>e27cab78-b6f3-4e4d-add6-d34d351070e4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TPA TC/TPA_Noticias</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
